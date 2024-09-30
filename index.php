89673b5eceff1293f934a129cbaeade7ad95b147
PaulMazeauEnsemble <paul@ensemble.ooo>
2024-09-30 15:09:38 +0200

    HomePage

--- index.php
+++ index.php
@@ -5,23 +5,29 @@ Template Name: HomePage
 
 include('header.php');
 
-// Récupérer les champs ACF
-$photo = get_field('image');
-$titre = get_field('titre');
+$image = get_field('image'); // Assurez-vous que le nom du champ est correct
+$Image_url = $image['url']; // Accédez à l'URL de l'image
+
+if ($Image_url):
+?>
+    <div class="md:col-span-6 flex justify-center items-center " style="background-image: url('<?php echo esc_url($Image_url); ?>'); background-size: cover; background-position: center; height: 100%; width: 100%;">
+    </div>
+<?php
+endif;
 ?>
 
-<div class="grid grid-cols-10 gap-10 h-screen">
-    <div class="col-span-4 flex items-end">
-        <h1 class="text-7xl text-white"><?php echo esc_html($titre); ?></h1>
-    </div>
-    <div class="col-span-6 h-full">
-        <?php if ($photo) : ?>
-            <img src="<?php echo esc_url($photo['url']); ?>" alt="<?php echo esc_attr($photo['alt']); ?>" class="w-full h-full object-cover" /> <!-- Afficher l'image -->
-        <?php endif; ?>
-    </div>
+<div class="content">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <div class="grid grid-cols-1 md:grid-cols-10 gap-4 h-screen">
+            <div class="md:col-span-4 flex items-end">
+                <h1 class="text-white text-7xl p-8"><?php the_field('titre'); ?></h1>
+            </div>
+            <div class="md:col-span-6 flex justify-center items-center " style="background-image: url('<?php echo esc_url($Image_url); ?>'); background-size: cover; background-position: center; height: 100%; width: 100%;">
+            </div>
+        </div>
+    <?php endwhile; endif; ?>
 </div>
 
 <?php
-// Inclusion du pied de page
 include('footer.php');
 ?>
