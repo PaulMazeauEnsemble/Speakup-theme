c9f3c0100d5bac51dc6fb573ae0251fa631332b4
PaulMazeauEnsemble <paul@ensemble.ooo>
2024-10-01 12:49:26 +0200

    page 404

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<div class="flex justify-center items-center h-screen flex-col text-white">
+    <p class="text-9xl font-Instrument">404</p>
+	<p class="text-xl font-Space">Cette page est introuvable.</p>
+</div>
+
+<?php get_footer(); ?>
