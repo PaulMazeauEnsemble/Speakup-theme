430ded528b7a9c40f47fb8b8c6e09813c9a30a71
PaulMazeauEnsemble <paul@ensemble.ooo>
2024-10-01 14:32:34 +0200

    ajout de la posibilité de télécharger un fichier son

--- components/talent-cards.php
+++ components/talent-cards.php
@@ -23,17 +23,27 @@ function display_talent_card($post_id) {
                 <?php $audio_counter = 1; ?>
                 <?php foreach ($audios as $audio) : ?>
                     <div class="custom-audio-player w-full mt-4">
-                        <div class="flex w-full">
-                            <button class="play-pause-btn">
-                                <svg class="play-icon w-3 h-3" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M8 5L0.5 9.33013L0.5 0.669873L8 5Z" fill="white"/>
-                                </svg>
-                                <svg class="pause-icon hidden w-3 h-3" viewBox="0 0 6 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <line x1="1" y1="4.37114e-08" x2="1" y2="9" stroke="white" stroke-width="2"/>
-                                    <line x1="5" y1="4.37114e-08" x2="5" y2="9" stroke="white" stroke-width="2"/>
-                                </svg>
-                            </button>
-                            <p class="text-white text-sm ml-2 uppercase font-DM font-thin">Extrait <?php echo $audio_counter; ?></p>
+                        <div class="flex w-full justify-between items-center">
+                            <div class="flex content-center">
+                                <button class="play-pause-btn">
+                                    <svg class="play-icon w-3 h-3" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M8 5L0.5 9.33013L0.5 0.669873L8 5Z" fill="white"/>
+                                    </svg>
+                                    <svg class="pause-icon hidden w-3 h-3" viewBox="0 0 6 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <line x1="1" y1="4.37114e-08" x2="1" y2="9" stroke="white" stroke-width="2"/>
+                                        <line x1="5" y1="4.37114e-08" x2="5" y2="9" stroke="white" stroke-width="2"/>
+                                    </svg>
+                                </button>
+                                <p class="text-white text-sm ml-2 uppercase font-DM font-thin">Extrait <?php echo $audio_counter; ?></p>
+                            </div>
+
+                            <div class="w-3 h-3">
+                                <a href="<?php echo esc_url($audio['url']); ?>" download>
+                                    <svg class="w-3 h-3" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M2.71716 9.28284C2.87337 9.43905 3.12663 9.43905 3.28284 9.28284L5.82843 6.73726C5.98464 6.58105 5.98464 6.32778 5.82843 6.17157C5.67222 6.01536 5.41895 6.01536 5.26274 6.17157L3 8.43431L0.737258 6.17157C0.581048 6.01536 0.327783 6.01536 0.171573 6.17157C0.0153632 6.32778 0.0153632 6.58105 0.171573 6.73726L2.71716 9.28284ZM2.6 0L2.6 9H3.4L3.4 0L2.6 0Z" fill="white"/>
+                                    </svg>
+                                </a>
+                            </div>
                         </div>
                         <div class="progress-bar bg-gray-700 mt-2 h-px flex items-center">
                             <div class="progress bg-white h-0.5"></div>
